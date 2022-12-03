272bdb55e984ef5de138fdea7f9c6a467b65566a
btramel <66852498+btramel@users.noreply.github.com>
2022-12-03 09:12:43 -0600

    new quips, test and run environments, no repeats, cronjob stops when list is empty

--- espnClient.js
+++ espnClient.js
@@ -1,30 +1,45 @@
 const { Client } = require('espn-fantasy-football-api/node-dev');
 
 // client
-const leagueClient = new Client({ leagueId: 39669613 });
+const twelveMan = new Client({ leagueId: 39669613 });
+const tenMan = new Client({ leagueId: 1501787134 });
+const time = new Date();
 let stinkers = [];
+let tenManStinkers = [];
+
+
 const getStinkers = async () => {
     try {
-        leagueClient.setCookies({ 
+        twelveMan.setCookies({ 
             espnS2: "AEB/SaRywbx9Drs1ZxLRBcr5O8yiXUUrznHHTfv/YUCOVjCrjTy8fsDWg6pr/c8jFEgbKXUCgrB+NMBkbsWm7JbiSKwFmLLyYeDI51bvU+y4tRCob66tThJoh0Qj032mO04mBb9h9PCx4W4U30KZkrCeAw76CbYZNdcUsa0vIB7oq4LGvYj+fcEJ3hv3vbMMk+57i7lIpmjwY4JO+oE+Fh/CGjzdQYRmN5RMYFQNEzp7nDXuTEw4lwAevqDdBOSR3orwkyR8clfVmg3tC58VGS8XilKuI/UbxZbhR8zkECM6crOE5RVWC4ybT8ggvzt+Kwz5XYGMGm2L/udzUx8Ud20t",
             SWID: "{11FB8C5C-FF63-47A1-9DCA-895047792509}",
         })
-        await leagueClient.getBoxscoreForWeek({ seasonId: 2022, matchupPeriodId: 12, scoringPeriodId: 12, }).then((teams) => {
+        tenMan.setCookies({
+            espnS2: "AECpuhbJzSQbgl0cF1MhPCek2n2Rl3Twe2i7l1oeZp9IL924ibHiyrIcO95HDckR2fka8x8eIwm2WN9vRc5i5ms%2B%2FFdGpT1YpP27QAdHCEoSSLdAHMxVJ7gPFe7vuD8bov0vtS2x8c3XR0GedYtGrZhgG5DRHdODrKMIrwfjSI0q1LOZyFLoWyKze6Jsw5%2FtQsVdEpklvN7InlE51%2F8PquQLvANcx6xPVOuVzgnLCW7NiICMUSFy%2BcD48Mm3m57y3%2BUf7oXCY2Q2vAi5yd%2FNR8h9jcLFLhxI6%2FDD9phRgqPjIZpFp9kTQt%2FxCiRn0o%2FSgIA%3D",
+            SWID: "{11FB8C5C-FF63-47A1-9DCA-895047792509}",
+        })
+        await twelveMan.getBoxscoreForWeek({ seasonId: 2022, matchupPeriodId: 12, scoringPeriodId: 12, }).then((teams) => {
             teams.filter((scorer) => {
                 if (scorer.homeTeamId === 1) {
                     scorer.homeRoster.forEach((player) => {
                         // console.log(player.player)
-                        let human = player.player.lastName;
+                        let human = player.player.fullName.toLowerCase();
+                        let lastName = player.player.lastName;
                         let points = player.totalPoints;
-                        (points > .01 && points < 8 && human != 'D/ST') ? stinkers.push(human) : stinkers = stinkers;
+                        (points > .01 && points < 8 && lastName != 'D/ST') 
+                            ? stinkers.push(human) 
+                            : stinkers = stinkers;
                         return stinkers
                     })
                 } else if (scorer.awayTeamId === 1) {
                     scorer.awayRoster.forEach((player) => {
                         // console.log(player.player)
-                        let human = player.player.lastName;
+                        let human = player.player.fullName.toLowerCase();
+                        let lastName = player.player.lastName;
                         let points = player.totalPoints;
-                        (points > .01 && points < 9 && human != 'D/ST') ? stinkers.push(human) : stinkers = stinkers;
+                        (points > .01 && points < 9 && lastName != 'D/ST') 
+                            ? stinkers.push(human) 
+                            : stinkers = stinkers;
                         return stinkers
                     })
                 }
@@ -33,12 +48,47 @@ const getStinkers = async () => {
             return stinkers
             });
 
+        await tenMan.getBoxscoreForWeek({ seasonId: 2022, matchupPeriodId: 12, scoringPeriodId: 12, }).then((teams) => {
+            teams.filter((scorer) => {
+                if (scorer.homeTeamId === 6) {
+                    scorer.homeRoster.forEach((player) => {
+                        // console.log(player.player)
+                        let human = player.player.fullName.toLowerCase();
+                        let lastName = player.player.lastName;
+                        let points = player.totalPoints;
+                        (points > .01 && points < 8 && lastName != 'D/ST') 
+                            ? tenManStinkers.push(human) 
+                            : tenManStinkers = tenManStinkers;
+                        return tenManStinkers
+                    })
+                } else if (scorer.awayTeamId === 6) {
+                    scorer.awayRoster.forEach((player) => {
+                        // console.log(player.player)
+                        let human = player.player.fullName.toLowerCase();
+                        let lastName = player.player.lastName;
+                        let points = player.totalPoints;
+                        (points > .01 && points < 9 && lastName != 'D/ST') 
+                            ? tenManStinkers.push(human) 
+                            : tenManStinkers = tenManStinkers;
+                        return tenManStinkers
+                    })
+                }
+            })
+            // console.log(tenManStinkers)
+            return tenManStinkers
+            });
+
+            const allStinkers = stinkers.concat(tenManStinkers)
+            console.log(allStinkers)
+            return allStinkers
+
+
     } catch (e) {
         console.error(e)
     }
-    return stinkers
 }
 
-const leagueClientSetup = leagueClient.readOnly;
+const twelveManSetup = twelveMan.readOnly;
+const tenManSetup = tenMan.readOnly;
 
-module.exports = { leagueClientSetup, getStinkers };
+module.exports = { twelveManSetup, tenManSetup, getStinkers };
--- index.js
+++ index.js
@@ -2,30 +2,60 @@ require("dotenv").config({ path: __dirname + "/.env" });
 const espnClient = require('./espnClient.js');
 const twitterClient = require("./twitterClient.js");
 const CronJob = require("cron").CronJob;
+const everySecond = "*/1 * * * * *"
+const everyHalfHour = "0 */30 * * * *"
             
 Promise.all([espnClient, twitterClient, CronJob]).then((values) => {
     espnClient.getStinkers().then((stinker) => {
-        const cronTweet = new CronJob("0 30 18 * * *", async () => {
-            const quips = [
-                " I guess", 
-                "",
-                " I suppose",
-                "",
-                " but you're also bad at this", 
-                "",
-                " but you did draft allen robinson", 
-                "",
-                " but maybe chase edmonds will be back soon",
-                "",
-                " but deon jackson will pop off one day",
-            ]
-            console.log(stinker)
-            let randomStinker = stinker[Math.floor(Math.random() * stinker.length)]
-            let randomQuip = quips[Math.floor(Math.random() * quips.length)]
+        const cronTweet = new CronJob(everySecond, async () => {
+            const films = [
+                "the tree of life",
+                "the godfather",
+                "the shining",
+                "vertigo",
+                "do the right thing",
+                "seven samurai",
+                "mad max fury road",
+                "come and see",
+                "raging bull",
+                "lord of the rings",
+            ];
+            let filmIndex = Math.floor(Math.random() * films.length)
+            let randomFilm = films[filmIndex]
 
-            await twitterClient.v2.tweet(`sorry about ${randomStinker} jxsn. it's been a weird week${randomQuip}. i hope toney scores 35 for you.`);
+            const quips = [
+                " imo.",
+                " I guess.", 
+                " I suppose.",
+                ", but you're also bad at this.", 
+                " but you did draft allen robinson.", 
+                ", but maybe chase edmonds will be back soon.",
+                ", but maybe deon jackson will pop off one day.",
+                ", but odds are you'll win a championship every twelve years!",
+                ". is there fantasy rocket league?",
+                ". have you looked at the waiver wire?",
+                " but criterion will always be there for you.",
+                `. maybe call it a day and watch ${films[randomFilm]} again?`
+            ];
+            let quipIndex = Math.floor(Math.random() * quips.length)
+            let randomQuip = quips[quipIndex]
+
+            let stinkerIndex = Math.floor(Math.random() * stinker.length)
+            let randomStinker = stinker[stinkerIndex]
+            
+            // TEST
+            stinker.length !== 0 
+                ? console.log(`sorry about ${randomStinker} jxsn. it's been a weird week${randomQuip}`) 
+                : cronTweet.stop()
+
+            stinker.splice(stinkerIndex, 1)
+            quips.splice(quipIndex, 1)
+
+            // RUN
+            // stinker.length !== 0 
+            //     ? await twitterClient.v2.tweet(`sorry about ${randomStinker} jxsn. it's been a weird week${randomQuip} i hope toney scores 35 for you.`)
+            //     : cronTweet.stop()
         })
         cronTweet.start()
-        cronTweet.stop()
     });
 })
